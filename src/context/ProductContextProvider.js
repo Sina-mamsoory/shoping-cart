cb1a06e92c0c76fcb94401aa2b4e28ff015a435f
sina mansoory <sinaman7979@gmail.com>
2023-02-08 20:24:16 +0330

    building a function for getting api and a productcontextprovider function

---
+++ src/context/ProductContextProvider.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { getProducts } from "../services/api";
+
+export const ProductContext = React.createContext();
+const ProductContextProvider = ({children}) => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async() => {
+            setProducts(await getProducts());
+        }
+
+        fetchAPI();
+    }, [])
+
+    return(
+        <div>
+            <ProductContext.Provider value={products}>
+                {children}
+            </ProductContext.Provider>
+        </div>
+    )
+}
+
+export default ProductContextProvider;
